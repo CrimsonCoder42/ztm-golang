31ada5451b571977d37e7ca49be917730cb1f727
CrimsonCoder42 <nostro37@gmail.com>
2023-09-01 09:23:48 -0400

    initial

--- src/lectures/demo/variables/variables.go
+++ src/lectures/demo/variables/variables.go
@@ -3,4 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Devin"
+	fmt.Println("My name is", myName, myName)
+
+	var name string = "Kathy"
+	fmt.Println("name = ", name)
+
+	userName := "Izzy"
+	fmt.Println("userName = ", userName)
+
 }
